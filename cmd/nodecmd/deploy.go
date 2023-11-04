f786c51f38cf3f8b1e07904cf8e811cd7da4b7d8
Felipe Madero <felipe.madero@gmail.com>
2023-11-04 18:17:45 -0300

    add node deploy cmd

---
+++ cmd/nodecmd/deploy.go
@@ -0,0 +1,101 @@
+// Copyright (C) 2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package nodecmd
+
+import (
+	"fmt"
+
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
+
+	"github.com/ava-labs/avalanche-cli/pkg/ansible"
+
+	"github.com/ava-labs/avalanche-cli/cmd/subnetcmd"
+	"github.com/ava-labs/avalanche-cli/pkg/models"
+	"github.com/ava-labs/avalanche-cli/pkg/ux"
+	"github.com/spf13/cobra"
+)
+
+func newDeployCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deploy [clusterName] [subnetName]",
+		Short: "(ALPHA Warning) Deploy a subnet into a devnet cluster",
+		Long: `(ALPHA Warning) This command is currently in experimental mode.
+
+The node deploy command deploys a subnet into a devnet cluster, creating subnet and blockchain txs for it.
+`,
+		SilenceUsage: true,
+		Args:         cobra.ExactArgs(2),
+		RunE:         deploySubnet,
+	}
+
+	return cmd
+}
+
+func deploySubnet(_ *cobra.Command, args []string) error {
+	clusterName := args[0]
+	subnetName := args[1]
+	if err := checkCluster(clusterName); err != nil {
+		return err
+	}
+	if err := setupAnsible(clusterName); err != nil {
+		return err
+	}
+	if _, err := subnetcmd.ValidateSubnetNameAndGetChains([]string{subnetName}); err != nil {
+		return err
+	}
+	notHealthyNodes, err := checkClusterIsHealthy(clusterName)
+	if err != nil {
+		return err
+	}
+	if len(notHealthyNodes) > 0 {
+		return fmt.Errorf("node(s) %s are not healthy yet, please try again later", notHealthyNodes)
+	}
+	incompatibleNodes, err := checkAvalancheGoVersionCompatible(clusterName, subnetName)
+	if err != nil {
+		return err
+	}
+	if len(incompatibleNodes) > 0 {
+		sc, err := app.LoadSidecar(subnetName)
+		if err != nil {
+			return err
+		}
+		ux.Logger.PrintToUser("Either modify your Avalanche Go version or modify your VM version")
+		ux.Logger.PrintToUser("To modify your Avalanche Go version: https://docs.avax.network/nodes/maintain/upgrade-your-avalanchego-node")
+		switch sc.VM {
+		case models.SubnetEvm:
+			ux.Logger.PrintToUser("To modify your Subnet-EVM version: https://docs.avax.network/build/subnet/upgrade/upgrade-subnet-vm")
+		case models.CustomVM:
+			ux.Logger.PrintToUser("To modify your Custom VM binary: avalanche subnet upgrade vm %s --config", subnetName)
+		}
+		return fmt.Errorf("the Avalanche Go version of node(s) %s is incompatible with VM RPC version of %s", incompatibleNodes, subnetName)
+	}
+	if err := deploy(clusterName, subnetName, models.Fuji); err != nil {
+		return err
+	}
+	ux.Logger.PrintToUser("Subnet successfully deployed into devnet!")
+	return nil
+}
+
+func deploy(clusterName, subnetName string, network models.Network) error {
+	subnetPath := "/tmp/" + subnetName + constants.ExportSubnetSuffix
+	if err := subnetcmd.CallExportSubnet(subnetName, subnetPath, network); err != nil {
+		return err
+	}
+	fmt.Println(subnetPath)
+	return nil
+	if err := ansible.RunAnsiblePlaybookExportSubnet(app.GetAnsibleDir(), app.GetAnsibleInventoryDirPath(clusterName), subnetPath, "/tmp", "all"); err != nil {
+		return err
+	}
+	hostAliases, err := ansible.GetAnsibleHostsFromInventory(app.GetAnsibleInventoryDirPath(clusterName))
+	if err != nil {
+		return err
+	}
+	untrackedNodes := []string{}
+	for _, host := range hostAliases {
+		// runs avalanche join subnet command
+		if err = ansible.RunAnsiblePlaybookTrackSubnet(app.GetAnsibleDir(), subnetName, subnetPath, app.GetAnsibleInventoryDirPath(clusterName), host); err != nil {
+			untrackedNodes = append(untrackedNodes, host)
+		}
+	}
+	return nil
+}
